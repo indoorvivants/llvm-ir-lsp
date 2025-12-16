9bcc7137a22a40f95c06a9cc92f12899501f8bc1
Anton Sviridov <keynmol@gmail.com>
2025-12-16 13:26:51 +0000

    wip

---
+++ project.scala
@@ -0,0 +1,11 @@
+// Main
+//> using scala "3.8.0-RC3"
+
+//> using dependency "com.indoorvivants.detective::platform::0.1.0"
+//> using dependency "com.lihaoyi::pprint:0.9.6"
+//> using dependency "org.typelevel::cats-parse::1.1.0"
+//> using dependency "tech.neander::langoustine-app::0.0.25"
+
+// Test
+//> using test.dependency "com.disneystreaming::weaver-cats::0.8.4"
+
